25ea1bffe069679c10c70e2afd9b248631fc1234
Marcin Kozak <marcinkozak005@gmail.com>
2020-12-12 20:15:27 +0100

    Add day 6 solutions

---
+++ 2020/6_1.py
@@ -0,0 +1,12 @@
+# https://adventofcode.com/2020/day/6
+# Input data was regexed so that each line is one group, and a '\n' was added in the last line
+
+f = open('6_1data.txt','r')
+lines = f.readlines()
+f.close()
+
+sum = 0
+for line in lines:
+    # last character in line is '\n'
+    sum += len(set(line[:-1]))
+print(sum)
---
+++ 2020/6_2.py
@@ -0,0 +1,17 @@
+# https://adventofcode.com/2020/day/6
+# Input: two empty lines ('\n') were added at the end of the file
+
+f = open('6_2data.txt','r')
+lines = f.readlines()
+f.close()
+
+sum = 0
+letters = 'abcdefghijklmnopqrstuvwxyz'
+tmp = set(letters)
+for line in lines:
+    if line != '\n':
+        tmp = tmp.intersection(set(line[:-1]))
+    else:
+        sum += len(tmp)
+        tmp = set(letters)
+print(sum)
