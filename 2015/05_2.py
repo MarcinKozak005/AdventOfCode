65c91a094ea6ff3ba9c78f74f76420fd07225722
Marcin Kozak <marcinkozak005@gmail.com>
2022-07-10 12:57:26 +0200

    Add 2015:05_2

---
+++ 2015/05_2.py
@@ -0,0 +1,26 @@
+
+file = open('05data.txt','r')
+
+nice = 0
+for line in file:
+    prev = ''
+    rule1_dict = {}
+    rule1 = False
+    char_2 = ''
+    rule2 = False
+    for i in range(len(line)):
+        char = line[i]
+        # rule 1
+        if not rule1:
+            if prev+char not in rule1_dict.keys():
+                rule1_dict[prev+char] = i
+            elif i - rule1_dict[prev+char] >= 2:
+                rule1 = True
+        # rule 2
+        if not rule2 and char_2 == char:
+            rule2 = True
+        char_2 = prev
+        prev = char
+    if rule1 and rule2:
+        nice += 1
+print(f'Total number of nice strings: {nice}')
