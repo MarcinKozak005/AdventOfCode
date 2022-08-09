a18c3619ae52ec82218f0df1c3cac3f96d6c1a03
Marcin Kozak <marcinkozak005@gmail.com>
2022-08-09 13:23:10 +0200

    Add 2015:19_2 Incomplete reductive BFS

--- 2015/19_2.py
+++ 2015/19_2.py
@@ -3,43 +3,36 @@ from collections import deque
 
 def get(key):
     try:
-        return formulas[key]
+        return past[key]
     except KeyError:
-        formulas[key] = []
-        return formulas[key]
+        return True
 
 def find():
     while True:
-        print(f'Queue length: {len(queue)}')
-        e,c = queue.popleft()
-        molecules = re.findall(r'[A-Z][a-z]?',e)
-        for i in range(len(molecules)):
-            if molecules[i] in formulas:
-                tmp = molecules[i]
-                for f in formulas[molecules[i]]:
-                    molecules[i] = f
-                    new_molecule = ''.join(molecules)
-                    if new_molecule == data:
-                        return c+1
-                    if len(new_molecule) <= result_length:
-                        queue.append((new_molecule,c+1))
-                molecules[i] = tmp
+        data,n_oper = queue.popleft()
+        print(f'L: {len(data)} N: {n_oper}')
+        if data == ['e']:
+            return n_oper
+        for i in range(len(data)):
+            for j in range(1,10):
+                compound = ''.join(data[i:i+j])
+                if compound in reversed_formulas:
+                    x = data[:i] + [reversed_formulas[compound]] + data[i+j:]
+                    if get(str(x)):
+                        queue.append((x, n_oper+1))
+                        past[str(x)] = False
 
 data = 'ORnPBPMgArCaCaCaSiThCaCaSiThCaCaPBSiRnFArRnFArCaCaSiThCaCaSiThCaCaCaCaCaCaSiRnFYFArSiRnMgArCaSiRnPTiTiBFYPBFArSiRnCaSiRnTiRnFArSiAlArPTiBPTiRnCaSiAlArCaPTiTiBPMgYFArPTiRnFArSiRnCaCaFArRnCaFArCaSiRnSiRnMgArFYCaSiRnMgArCaCaSiThPRnFArPBCaSiRnMgArCaCaSiThCaSiRnTiMgArFArSiThSiThCaCaSiRnMgArCaCaSiRnFArTiBPTiRnCaSiAlArCaPTiRnFArPBPBCaCaSiThCaPBSiThPRnFArSiThCaSiThCaSiThCaPTiBSiRnFYFArCaCaPRnFArPBCaCaPBSiRnTiRnFArCaPRnFArSiRnCaCaCaSiThCaRnCaFArYCaSiRnFArBCaCaCaSiThFArPBFArCaSiRnFArRnCaCaCaFArSiRnFArTiRnPMgArF'
-result_length = len(data)
 
-formulas = {}
+past = {}
+reversed_formulas = {}
 file = open('19data.txt')
+
+queue = deque([(re.findall(r'[A-Z][a-z]?', data),0)])
 
 # Read data
 for line in file:
     m = re.findall(r'[a-zA-Z]+', line)
-    get(m[0]).append(m[1])
+    reversed_formulas[m[1]] = m[0]
 
-# Calculate
-queue = deque([(x,1) for x in formulas['e']])
-
-print(f'Result {find()}')
-
-
-
+print(f'Result: {find()}')
